703494b84d98e9bf9dc179b9da5d7f7898839e60
Apower11 <59498034+Apower11@users.noreply.github.com>
2021-06-17 17:03:28 +0100

    Version 2.0.1

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,14 +3,14 @@ import { Navbar, Nav, NavDropdown, Form, FormControl, Button } from 'react-boots
 export default function NavbarComponent(props) {
     return (
         <Navbar collapseOnSelect expand="lg" bg="primary" variant="dark">
-  <Navbar.Brand href="#home">Simply Sum</Navbar.Brand>
+  <Navbar.Brand href="/">Simply Sum</Navbar.Brand>
   <Navbar.Toggle aria-controls="responsive-navbar-nav" />
   <Navbar.Collapse id="responsive-navbar-nav">
     <Nav className="mr-auto">
       {/* <Nav.Link href="#features">Features</Nav.Link>
       <Nav.Link href="#pricing">Pricing</Nav.Link> */}
       <NavDropdown title="A Level Maths" id="collasible-nav-dropdown">
-        <NavDropdown.Item href="#action/3.1">Past Papers</NavDropdown.Item>
+        <NavDropdown.Item href="/">Past Papers</NavDropdown.Item>
         {/* <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
         <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
         <NavDropdown.Divider />
